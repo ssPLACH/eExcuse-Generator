ce5888c48b7adcc5b88edb112426eb65237cbad8
ssPLACH <134180679+ssPLACH@users.noreply.github.com>
2023-11-02 18:54:21 +0000

    array e input creados, falta incluir math random para el innerhtml

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = who + action + what + when;
+  console.log(a[Math.floor(Math.random() * arr.length)]);
 };
+//write your code here
+let who = ["The cat", "My son", "His rabit", "My crazy grandmother"];
+let action = ["ate", "peed", "pop in", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "just before i had to live",
+  "right on time for school",
+  "when I finished breakast",
+  "while I was in the shower"
+];
